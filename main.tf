e19b24d4b5f60267c547edba6c8066996dc48190
Major Hayden <major@mhtx.net>
2023-02-14 16:57:10 -0600

    Add ACL and website config for bucket

--- main.tf
+++ main.tf
@@ -23,6 +23,19 @@ provider "aws" {
   }
 }
 
-resource "aws_s3_bucket" "cloudx_image_locator_json" {
+resource "aws_s3_bucket" "cloudx_json_bucket" {
   bucket = "cloudx-json-bucket"
+}
+
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+  bucket = aws_s3_bucket.cloudx_json_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_website_configuration" "cloudx_testing" {
+  bucket = aws_s3_bucket.cloudx_json_bucket.bucket
+
+  index_document {
+    suffix = "index.json"
+  }
 }
