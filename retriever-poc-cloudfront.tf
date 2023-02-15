021dd7880be23932a986e430cf0cfcb8939e3109
Major Hayden <major@mhtx.net>
2023-02-15 12:54:13 -0600

    Fix bucket name

--- retriever-poc-cloudfront.tf
+++ retriever-poc-cloudfront.tf
@@ -49,7 +49,7 @@ resource "aws_cloudfront_distribution" "retriever_poc" {
 
   logging_config {
     include_cookies = false
-    bucket          = aws_s3_bucket.cloudx_json_bucket.bucket
+    bucket          = aws_s3_bucket.cloudx_json_bucket.bucket_domain_name
     prefix          = "logs"
   }
 
