46fd0fa373972d7d3291f456e74bb43338a6788c
Major Hayden <major@mhtx.net>
2023-02-15 08:22:37 -0600

    Rename ACL/website resources to match

--- retriever-poc.tf
+++ retriever-poc.tf
@@ -5,12 +5,12 @@ resource "aws_s3_bucket" "cloudx_json_bucket" {
   bucket = "cloudx-json-bucket"
 }
 
-resource "aws_s3_bucket_acl" "example_bucket_acl" {
+resource "aws_s3_bucket_acl" "cloudx_json_bucket" {
   bucket = aws_s3_bucket.cloudx_json_bucket.id
   acl    = "private"
 }
 
-resource "aws_s3_bucket_website_configuration" "cloudx_testing" {
+resource "aws_s3_bucket_website_configuration" "cloudx_json_bucket" {
   bucket = aws_s3_bucket.cloudx_json_bucket.bucket
 
   index_document {
