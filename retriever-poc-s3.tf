c0c90ed04832e609023b635e210b1244204b2d0d
fkolwa <fkolwa@redhat.com>
2023-03-20 08:50:01 +0100

    remove s3 website endpoint

--- retriever-poc-s3.tf
+++ retriever-poc-s3.tf
@@ -11,13 +11,3 @@ resource "aws_s3_bucket_acl" "cloudx_json_bucket" {
   bucket = aws_s3_bucket.cloudx_json_bucket.id
   acl    = "private"
 }
-
-# Allow the bucket to host a website.
-resource "aws_s3_bucket_website_configuration" "cloudx_json_bucket" {
-  bucket = aws_s3_bucket.cloudx_json_bucket.bucket
-
-  index_document {
-    suffix = "index.json"
-  }
-}
-
