cd709f23e53982c280217325382150554d3141ca
Major Hayden <major@redhat.com>
2023-03-29 13:27:48 -0500

    Add GitHub Pages challenge DNS record

--- monitoring.tf
+++ monitoring.tf
@@ -179,3 +179,20 @@ resource "aws_route53_record" "github_pages_upptime" {
 
   ttl = "3600"
 }
+
+# Verify status domain for GitHub Pages.
+resource "aws_route53_record" "github_pages_upptime_verification" {
+  zone_id = data.aws_route53_zone.imagedirectory_cloud.zone_id
+  name    = "_github-pages-challenge-redhatcloudx.status"
+  type    = "TXT"
+
+  records = [
+    "6032bd6825549735214b9280b7f1e6",
+  ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  ttl = "3600"
+}
