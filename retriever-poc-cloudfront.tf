b15efafc91cd83c8f395e4bedf3716561eea42bf
fkolwa <fkolwa@redhat.com>
2023-03-31 08:22:41 +0200

    Add custom error response Necessary to enable proper routing on static one-page web applications

--- retriever-poc-cloudfront.tf
+++ retriever-poc-cloudfront.tf
@@ -108,6 +108,13 @@ resource "aws_cloudfront_distribution" "retriever_poc" {
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
   }
+
+  custom_error_response {
+    error_caching_min_ttl = 300
+    error_code = 404
+    response_code = 200
+    response_page_path = "/index.html"
+  }
 }
 
 # Add a DNS record for the CloudFront distribution.
