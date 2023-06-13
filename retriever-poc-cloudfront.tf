1f6da0626536f5e2a494beb911621b3be66fbf0e
Major Hayden <major@redhat.com>
2023-06-13 08:53:39 -0500

    Fix terraform formatting

--- retriever-poc-cloudfront.tf
+++ retriever-poc-cloudfront.tf
@@ -111,9 +111,9 @@ resource "aws_cloudfront_distribution" "retriever_poc" {
 
   custom_error_response {
     error_caching_min_ttl = 300
-    error_code = 404
-    response_code = 200
-    response_page_path = "/index.html"
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/index.html"
   }
 }
 
--- staging-cloudfront.tf
+++ staging-cloudfront.tf
@@ -109,9 +109,9 @@ resource "aws_cloudfront_distribution" "cid_staging" {
 
   custom_error_response {
     error_caching_min_ttl = 300
-    error_code = 404
-    response_code = 200
-    response_page_path = "/index.html"
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/index.html"
   }
 }
 
